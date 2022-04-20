fb61aad2600d86da4941db3cba033e107439c28d
AndreFelipeMoura <andre.fa.moura@outlook.com>
2022-04-19 21:19:07 -0300

    Primeiro commit

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+// Nome - conter "espaço" entre nome e sobrenome para validar. (cadastro)
+// Email - conter "@" e terminar ".com" para validar. (login/cadastro/rec . senha)
+// Senha - Conter "caracter$#%" e/ou "letra maiúscula" e/ou "número" para validar. (login/cadastro/rec . senha)
+// Confirmação de senha - Conter "caracter$#%" e/ou "letra maiúscula" e/ou "número" para validar. (cadastro/rec . senha)
+// RG - 8.797.606-0 Oito numeros ,com "traço - " no penultimo para validar.(cadastro)
+// Cep - Apicep completa os demais campos abaixo: (API ViaCEP - https://viacep.com.br/)(cadastro)
+// Campos de endereço - (estado, cidade, bairro, rua, número, complemento).(cadastro)
+// prompts/alerts
